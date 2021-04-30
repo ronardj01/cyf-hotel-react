3487b462959deb2ed7e8e36a0fe405e139b2ba2d
ronardj01 <ronardj01@gmail.com>
2021-04-30 22:47:04 +0200

    Extrated the header and added an image

---
+++ src/components/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <header className="App-header">
+      <img
+        id="headerImage"
+        alt="CYF Logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+      CYF Hotel
+    </header>
+  );
+}
+
+export default Heading;
