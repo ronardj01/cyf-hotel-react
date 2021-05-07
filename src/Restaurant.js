708f1f60edbecb27ab0e58131eb92d8eb6587256
ronardj01 <ronardj01@gmail.com>
2021-05-07 11:14:57 +0200

    Rendered the restaurant component

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,10 +3,10 @@ import React from "react";
 const Restaurant = () => {
   const pizzas = 0;
   return (
-    <div>
+    <div className="container mt-2">
       <h3>Restaurant Orders</h3>
-      <ul>
-        <li>
+      <ul className="list-group-flush">
+        <li className="list-group-item">
           Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
         </li>
       </ul>
