a7498ed13f346192e712fea6cea0c14877e95382
ronardj01 <ronardj01@gmail.com>
2021-05-07 18:51:20 +0200

    Redered more orders

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -3,11 +3,15 @@ import React from "react";
 import Order from "./components/Order";
 
 const Restaurant = () => {
+  const orderTypes = ["Pizzas", "Salads", "Chocolate cake"];
+
   return (
     <div className="container mt-5">
       <h3>Restaurant Orders</h3>
       <ul className="list-group-flush">
-        <Order />
+        {orderTypes.map(orderType => (
+          <Order orderType={orderType} />
+        ))}
       </ul>
     </div>
   );
